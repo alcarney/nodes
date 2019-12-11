8ec50157e25d284a8f3381a67207a058ae4ff624
Alex Carney <alcarneyme@gmail.com>
2019-12-11 19:11:50 +0000

    Split the 'library' from the 'app'

--- src/main.ts
+++ src/main.ts
@@ -199,17 +199,3 @@ class Canvas {
     this.canvas.appendChild(g)
   }
 }
-
-const main = document.getElementById("canvas")
-
-if (main === null) {
-  throw new Error("Could not find canvas element")
-}
-
-const canvas = new Canvas(main, 100)
-
-const grid = new Grid(canvas.height, 25)
-canvas.setBackground(grid)
-
-const node = new EditorNode()
-canvas.addItem(node)
